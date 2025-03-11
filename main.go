65d73a7975b30f32f3f42f6c9e1e11afe2baccf8
Pritesh Tupe <mail@priteshtupe.com>
2025-03-11 17:36:42 -0400

    Add a working implementation

--- main.go
+++ main.go
@@ -12,24 +12,34 @@ type Options struct {
 	Title string
 }
 
-var templates = template.Must(template.ParseFiles("./public/index.html", "./public/results.html"))
+var templates = template.Must(template.ParseFiles(
+	"./public/index.html",
+	"./public/results.html",
+))
 
 func main() {
-	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			templates.Execute(w, "index.html")
+		} else if r.Method == http.MethodPost {
+			items, err := queryYoutube(r.FormValue("q"))
+			if err != nil {
+				fmt.Println(err)
+				// TODO: expand on error page
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+
+			err = templates.ExecuteTemplate(w, "results.html", items)
+			if err != nil {
+				fmt.Println("Error : ", err)
+				w.WriteHeader(http.StatusInternalServerError)
+			}
+		} else {
+			w.WriteHeader(http.StatusBadRequest)
+		}
+	})
 
 	fmt.Println(time.Now(), " Server listening on port 8080...")
 	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-func rootHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		templates.Execute(w, "index.html")
-	} else if r.Method == http.MethodPost {
-		results := r.FormValue("query")
-		if err := templates.ExecuteTemplate(w, "results.html", &struct{ Results string }{
-			Results: "You searched for: " + results,
-		}); err != nil {
-			fmt.Println("Error : ", err)
-		}
-	}
 }
